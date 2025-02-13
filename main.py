a9aaa2691622463d66d06d6f3c514f77dafde53c
resazm <hiroaki.higashi.res@gmail.com>
2025-02-13 14:47:25 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Streamlit App", page_icon=":shark:")
+st.set_page_config(page_title="Streamlit App", page_icon=":Bar Chart:")
 
 st.title("RES売上データ集計")
 st.write("written in Python by higashi 2024.2")
