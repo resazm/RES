b5437604b0ffd2366c8020d00b9c731513075894
resazm <hiroaki.higashi.res@gmail.com>
2025-03-31 10:34:42 +0900

    20250331

--- pages/00_(当月)DBデータダッシュボード.py
+++ pages/00_(当月)DBデータダッシュボード.py
@@ -163,7 +163,7 @@ with col1:
     pivot_table
 with col2:
     fig = px.bar(tougetu_df .reset_index(), x="日付", y="合計金額", color="新 業務提携者（従属）", title="金額", barmode="stack")
-    fig.update_yaxes(tickformat=",",dtick=200000)#,range=(0, 2000000)
+    fig.update_yaxes(tickformat=",")#,range=(0, 2000000)
     fig.update_xaxes(dtick=2,range=(0, 31))
     st.subheader("")
     st.plotly_chart(fig, use_container_width=True)
@@ -189,7 +189,7 @@ with col1:
     pivot_table
 with col2:
     fig = px.bar(tougetu_df .reset_index(), x="日付", y="数量", color="新 業務提携者（従属）", title="数量", barmode="stack")
-    fig.update_yaxes(tickformat=",",dtick=2)
+    fig.update_yaxes(tickformat=",")
     fig.update_xaxes(dtick=2,range=(0, 31))
     st.subheader("")
     st.plotly_chart(fig, use_container_width=True)
