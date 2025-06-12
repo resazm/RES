19337ff04649e3d8fbe8773bbc72bf48f9161850
resazm <hiroaki.higashi.res@gmail.com>
2025-06-12 18:00:32 +0900

    1800

--- ツール/100_python.py
+++ ツール/100_python.py
@@ -6,7 +6,7 @@ pg = st.navigation([st.Page("100_メディア別ニュース取得ツール1.py"
                     st.Page("102_銀行口座ウォーターフォールチャート作成ツール.py"), 
                     st.Page("103_日経平均.py"), 
                     st.Page("104_yfinanceデータ取得.py"), 
-                    st.Page("105_LP分析.py"),
+                    #st.Page("105_LP分析.py"),
                     st.Page("106_ランキング.py"),
                     st.Page("107_経済指標.py")])
 pg.run()
