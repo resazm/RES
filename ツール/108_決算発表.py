495c389acaeac8b35a9b1eed06848c8548796713
resazm <hiroaki.higashi.res@gmail.com>
2025-06-13 13:35:26 +0900

    06131335

--- ツール/108_決算発表.py
+++ ツール/108_決算発表.py
@@ -9,7 +9,7 @@ st.title("決算発表予定企業検索")
 def fetch_companies(date_str):
     url = f"https://www.traders.co.jp/market_jp/earnings_calendar/all/all/1?term={date_str}"
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
     }
     try:
         res = requests.get(url, headers=headers, timeout=15)
